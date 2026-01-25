41d7ac74f6c339a59f3f4cb8f2a75afc1cf74749
Arpit Verma <arpitv970@gmail.com>
2026-01-25 22:08:30 +0530

    fixed resume sec

--- apps/web/src/components/shared/copy-download.tsx
+++ apps/web/src/components/shared/copy-download.tsx
@@ -1,7 +1,72 @@
-import { Button } from "../ui/button"
+import { Button } from "@/components/ui/button"
+import { ButtonGroup } from "@/components/ui/button-group"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  ChevronDownIcon,
+  CopyIcon,
+  DownloadIcon
+} from "lucide-react"
+import { toast } from "sonner"
 
-export const CopyDownload = () => {
+enum OptionEnum {
+  COPY_MD = 'Copy as Markdown',
+  DOWNLOAD_MD = 'Download as Markdown',
+  DOWNLOAD_PDF = 'Download as PDF'
+}
+
+interface ICopyDownload {
+  /** raw markdown content */
+  content: string;
+
+  /** optional filename */
+  filename?: string;
+}
+
+export const CopyDownload: React.FC<ICopyDownload> = ({ content, filename }) => {
+
+  const handleAction = async (opts: OptionEnum) => {
+    if (opts === OptionEnum.COPY_MD) {
+      await navigator.clipboard.writeText(content)
+      toast.success("Markdown copied to clipboard");
+    } else if (opts === OptionEnum.DOWNLOAD_MD) {
+
+    } else if (opts === OptionEnum.DOWNLOAD_PDF) {
+
+    }
+  }
+
   return (
-    <Button>Copy as Markdown</Button>
+    <ButtonGroup>
+      <Button onClick={() => handleAction(OptionEnum.COPY_MD)} variant="outline">
+        <CopyIcon />
+        {OptionEnum.COPY_MD}
+      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="!pl-2">
+            <ChevronDownIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-full">
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={() => handleAction(OptionEnum.DOWNLOAD_MD)}>
+              <DownloadIcon />
+              {OptionEnum.DOWNLOAD_MD}
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => handleAction(OptionEnum.DOWNLOAD_PDF)}>
+              <DownloadIcon />
+              {OptionEnum.DOWNLOAD_PDF}
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </ButtonGroup>
+
   )
 }
